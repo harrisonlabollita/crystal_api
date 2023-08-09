b828c974c27560c3d22616580a48fa81fa536cc1
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2023-08-09 15:46:29 -0700

    database bootstraps

--- data/bootstrap.py
+++ data/bootstrap.py
@@ -1,72 +1,88 @@
-import json 
-import random
+import json
 import time
 import os
+import requests
+import zipfile
+import shutil
 
-raw_data : str = "cifdata.txt"
+import sqlite3
 
-def buildDatabase() -> None:
+DATA_PATH: str = "cif/cifdata.txt" # encoded in latin1
+URL : str = "http://rruff.geo.arizona.edu/AMS/zipped_files/cif_archive_2023_07_30.zip"
 
-    file = open("cifdata.txt", "r", encoding="latin1")
+def fetch_data() -> None:
+    print('fetching data...')
+    response = requests.get(URL)
+    if response.status_code == 200:
+        print('fetch successful...writing zip file')
+        with open('data.zip', 'wb') as file: file.write(response.content)
+    print('unzipping data to dir cif')
+    with zipfile.ZipFile('data.zip',  'r') as zipref: zipref.extractall('cif')
+
+def cleanup() -> None:
+    print('cleaning up from bootstrap...')
+    if os.path.isfile('data.zip'): os.remove('data.zip')
+    if os.path.isdir('cif'): shutil.rmtree('cif')
+
+
+def build_database() -> None:
+
+    print('building sql database...')
+
+    file = open(DATA_PATH, "r", encoding="latin1")
     lines = file.readlines()
     file.close()
 
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS materials (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        lattice TEXT,
+        volume REAL,
+        atoms TEXT,
+        sgroup TEXT,
+        source TEXT
+        )
+    ''')
+
     ciffile = []
-    numid = 10000
     for (il, line) in enumerate(lines):
-
         if "END" not in line:
-
             ciffile.append(line)
-
-            if "chemical_name_mineral" in line:
-                name = line.split("_chemical_name_mineral")[-1].replace("'", "").strip()
-            if "cell_length_a" in line:
-                a = float(line.split()[-1].replace(",", ""))
-            if "cell_length_b" in line:
-                b = float(line.split()[-1].replace(",", ""))
-            if "cell_length_c" in line:
-                c = float(line.split()[-1].replace(",", ""))
-            if "cell_angle_alpha" in line:
-                alpha = float(line.split()[-1])
-            if "cell_angle_beta" in line:
-                beta = float(line.split()[-1])
-            if "cell_angle_gamma" in line:
-                gamma = float(line.split()[-1])
-            if "cell_volume" in line:
-                volume = float(line.split()[-1])
-            if "symmetry_space_group_name" in line:
-                sgroup = line.split("symmetry_space_group_name_H-M")[-1]
-                sgroup = sgroup.replace(",", "").replace(" ", "").replace("'", "")
+            if "chemical_name_mineral" in line: name = line.split("_chemical_name_mineral")[-1].replace("'", "").strip()
+            if "cell_length_a" in line: a = float(line.split()[-1].replace(",", ""))
+            if "cell_length_b" in line: b = float(line.split()[-1].replace(",", ""))
+            if "cell_length_c" in line: c = float(line.split()[-1].replace(",", ""))
+            if "cell_angle_alpha" in line: alpha = float(line.split()[-1])
+            if "cell_angle_beta" in line: beta = float(line.split()[-1])
+            if "cell_angle_gamma" in line: gamma = float(line.split()[-1])
+            if "cell_volume" in line: volume = float(line.split()[-1])
+            if "symmetry_space_group_name" in line: 
+                sgroup = line.split("symmetry_space_group_name_H-M")[-1].strip().replace(",", "").replace("'", "")
             if "chemical_formula_sum" in line:
                 line = line.replace("(", "").replace(")", "").replace("'", "")
-                species = list(map(lambda x : "".join([s.replace('.', '') for s in x if not s.isnumeric() ]), line.split()[1:]))
-                species = " ".join(species)
+                atoms = list(map(lambda x : "".join([s.replace('.', '') for s in x if not s.isnumeric() ]), line.split()[1:]))
+                atoms = " ".join(atoms)
         else:
             ciffile.append(line)
-            Crystal = {}
-
-            Crystal['id']      = numid
-            assert name != '', f"Error parsing name {name}, check line numer = {il}"
-            Crystal['name']    = name
-            Crystal['lattice'] = [a,b,c, alpha, beta, gamma]
-            Crystal['volume']  = volume
-            Crystal['sgroup']  = sgroup
-            Crystal['atoms']   = species
-            Crystal['file']    = "".join(ciffile)
-            with open(str(Crystal['id'])+'.json', 'w') as file:
-                json.dump(Crystal, file)
-            numid += 1
+            lattice = json.dumps([a,b,c, alpha, beta, gamma])
+            source    = "".join(ciffile)
+            # write to database
+            cursor.execute('''
+                INSERT INTO materials (name, lattice, volume, atoms, sgroup, source)
+                VALUES (?, ?, ?, ?, ?, ?)
+            ''', (name, lattice, volume, atoms, sgroup, source)
+            )
             ciffile = []
+    conn.commit()
+    conn.close()
 
 if __name__ == "__main__":
+    fetch_data()
+    build_database()
+    cleanup()
 
-    BUILD = int(os.getenv("BUILD")) if os.getenv("BUILD") is not None else 0
 
-    if BUILD:
-        print("You have elected to build the json representation of the database.\nWaiting 5 seconds before starting...")
-        time.sleep(5)
-        start = time.time()
-        buildDatabase()
-        stop = time.time()
-        print(f"Finished building database in {(stop-start)} seconds")
